13ab5bf2d16bf29216e5e5c951dd8b266eca9799
reasat-rafio <rafioar96@gmail.com>
2021-01-16 00:11:29 +0600

    final fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { Layout } from "../components/Layout/Layout";
-import "../styles/globals.css";
+import "../styles/tailwind.css";
 import { AnimatePresence } from "framer-motion";
 import { GlobalProvider } from "../store/GlobalStore";
 
